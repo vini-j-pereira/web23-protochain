b26cd69e9c9df9617c744308a697ad5cac97c0e7
vini-j-pereira <162238067+vini-j-pereira@users.noreply.github.com>
2025-10-16 23:37:32 -0300

    comentarios

--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,12 +1,13 @@
 import sha256 from 'crypto-js/sha256';
 import Validation from './validation';
 
-export default class Block {  // Criação da Classe Block
-    index: number;
-    timestamp: number;        //Criação de seu Atributos
-    hash: string;
-    previousHash: string;
-    data: string;
+export default class Block {   // Iniciando a Classe Block
+                              // Criação de seu Atributos
+    index: number;           // O Index é o numero referente a possição do Bloco na chain
+    timestamp: number;      // Otimestamp é uma representação numérica de um ponto no tempo, especificico refrente ao "Época do Unix" 
+    hash: string;          // O hash é a impressão digital do bloco que garante sua segurança e ligação na blockchain.
+    previousHash: string; // Cada bloco tem seu próprio hash e o hash do bloco anterior, o que mantém todos ligados e garante a segurança da blockchain. 
+    data: string;        // O data e responsalve por receber e armazenar todos os dados de um bloco  
 
     constructor(block?: Block){ // O constructor serve para indicar como sera inicializado o nosso objeto  
         this.index = block?.index || 0;
