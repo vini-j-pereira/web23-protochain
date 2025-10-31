3aa40b7626ab8a092fe25abb07fc78cc61387efe
vini-j-pereira <vinicius_pereira13@estudante.sesisenai.org.br>
2025-10-31 20:35:13 -0300

    create wallet and client wallet

---
+++ src/client/walletClient.ts
@@ -0,0 +1,4 @@
+import Wallet from "../lib/wallet";
+
+const wallet = new Wallet("68d76daa78e2f19122bb5b309225a6b5ad6a03f83b308f9d4361943562a43d0b");
+console.log(wallet);
---
+++ src/lib/wallet.ts
@@ -0,0 +1,32 @@
+import * as ecc from 'tiny-secp256k1';
+import ECPairFactory, { ECPairInterface } from 'ecpair';
+
+const ECPair = ECPairFactory(ecc);
+
+export default class Wallet {
+
+    privateKey: string;
+    publicKey: string;
+
+    constructor(wifOrPrivateKey?: string) {
+        let keys;
+
+        if (wifOrPrivateKey) {
+            
+            if (wifOrPrivateKey.length === 64) {
+                keys = ECPair.fromPrivateKey(Buffer.from(wifOrPrivateKey, "hex"));
+
+            } else {
+                keys = ECPair.fromWIF(wifOrPrivateKey);
+            }
+
+        } else {
+            keys = ECPair.makeRandom();
+        }
+
+
+        this.privateKey = keys.privateKey?.toString("hex") || "";
+        this.publicKey = keys.publicKey.toString("hex");
+    }
+
+}
