0864bb2b153ffa2a7db57cf34b67496c4a122594
vini-j-pereira <vinicius_pereira13@estudante.sesisenai.org.br>
2026-03-15 12:52:56 -0300

    Class TransactionOutout

--- src/lib/transaction.ts
+++ src/lib/transaction.ts
@@ -2,51 +2,74 @@ import TransactionType from "./transactionType";
 import TransactionInput from "./transactionInput";
 import sha256 from 'crypto-js/sha256';
 import Validation from "./validation";
+import TransactionOutput from "./transactionOutput";
 
-export default class Transaction{
+export default class Transaction {
     type: TransactionType;
     timestamp: number;
     hash: string;
-    txInput: TransactionInput | undefined;
-    to: string;
+    txInputs: TransactionInput[] | undefined;
+    txOutputs: TransactionOutput[];
 
     constructor(tx?: Transaction) {
         this.type = tx?.type || TransactionType.REGULAR;
         this.timestamp = tx?.timestamp || Date.now();
-        this.to = tx?.to || "";
 
-        if(tx && tx.txInput){
-          this.txInput = new TransactionInput(tx.txInput);
+        this.txInputs = tx?.txInputs
+            ? tx.txInputs.map(txi => new TransactionInput(txi))
+            : undefined;
 
-        } else {
-            this.txInput = undefined
-        }
-        
+        this.txOutputs = tx?.txOutputs
+            ? tx.txOutputs.map(txo => new TransactionOutput(txo))
+            : [];
+
         this.hash = tx?.hash || this.getHash();
-        
+
+        this.txOutputs.forEach((txo, index, arr) => arr[index].tx = this.hash);
+
 
     }
 
-    getHash() : string {
-        const from = this.txInput ? this.txInput.getHash() : "";
-        return sha256(this.type + from + this.to + this.timestamp).toString();
+    getHash(): string {
+        const from = this.txInputs && this.txInputs.length
+            ? this.txInputs.map(txi => txi.signature).join(",")
+            : "";
+
+        const to = this.txOutputs && this.txOutputs.length
+            ? this.txOutputs.map(txo => txo.getHash()).join(",")
+            : "";
+
+        return sha256(this.type + from + to + this.timestamp).toString();
     }
 
-    isValid() : Validation {
-        if(this.hash !== this.getHash()){
+    isValid(): Validation {
+        if (this.hash !== this.getHash()) {
             return new Validation(false, "Invalid hash!");
         }
 
-        if(!this.to){
-            return new Validation(false, "Invalid data!");
+        if (!this.txOutputs || !this.txOutputs.length || this.txOutputs.map(txo => txo.isValid()).some(v => !v.success)) {
+            return new Validation(false, "Invalid TXO!");
         }
 
-        if(this.txInput) {
-            const validation = this.txInput.isValid();
-            if(!validation.success){
-                return new Validation(false, `Invalid tx: ${validation.message}`);
+        if (this.txInputs && this.txInputs.length) {
+            const validations = this.txInputs.map(txi => txi.isValid()).filter(v => !v.success);
+            if (validations && validations.length) {
+                const message = validations.map(v => v.message).join(" ");
+                return new Validation(false, `Invalid tx: ${message}`);
+            }
+
+            const inputSum = this.txInputs.map(txi => txi.amount).reduce((a, b) => a + b, 0);
+            const inputOutput = this.txOutputs.map(txo => txo.amount).reduce((a, b) => a + b, 0);
+            if(inputSum < inputOutput){
+                return new Validation(false, `Invalid tx: input amounts must be equals or greater than outputs amounts. `);
             }
         }
+
+        if(this.txOutputs.some(txo => txo.tx !== this.hash)){
+            return new Validation(false, `Invalid TXO reference hash.`);
+        }
+
+        //TODO: validar as taxas e recompensas quando tx.type === FEE;
 
         return new Validation();
     }
